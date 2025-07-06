3616fc513800e37a4445fda6adb497134fece304
Anna Vasilevich <dzichonka@gmail.com>
2025-07-06 14:49:33 +0200

    feat: add search section

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,7 @@
 import RefreshButton from './RefreshButton';
 
 export const ErrorPage = () => (
-  <div>
+  <div className="section flex flex-col items-center justify-center gap-4">
     <h1>Something went wrong</h1>
     <p>Try refreshing the page.</p>
     <RefreshButton />
