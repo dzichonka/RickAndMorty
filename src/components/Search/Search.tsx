502dbb612618cdb654b6ef14f2270b1bc8e05a14
Anna Vasilevich <dzichonka@gmail.com>
2025-07-06 16:20:28 +0200

    feat: implement saving of search in LS

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,7 +2,16 @@ import { Component } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 class Search extends Component {
-  state = {};
+  state = {
+    search: localStorage.getItem('lastSearch') || '',
+  };
+
+  handleSearch = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    if (this.state.search.trim()) {
+      localStorage.setItem('lastSearch', this.state.search.trim());
+    }
+  };
   render() {
     return (
       <div className="section flex flex-row items-center justify-center">
@@ -10,11 +19,13 @@ class Search extends Component {
           <input
             type="text"
             id="search"
+            value={this.state.search}
+            onChange={(event) => this.setState({ search: event.target.value })}
             className="w-full px-2 py-1 border border-violet-500 rounded
             focus:outline-none focus:ring-violet-200 focus:ring"
           />
         </label>
-        <button className="btn-icon">
+        <button className="btn-icon" onClick={this.handleSearch}>
           <BsSearch />
         </button>
       </div>
