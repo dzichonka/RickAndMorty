df28781c035664be9a15b425d4a7c1a072846a4d
Anna Vasilevich <dzichonka@gmail.com>
2025-08-08 13:29:00 +0200

    feat: add ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,22 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from '@/Router/router';
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary.tsx';
 import ThemeProvider from './contexts/theme/ThemeProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const rootElement = document.createElement('div');
 rootElement.id = 'root';
 document.body.appendChild(rootElement);
 
+const queryClient = new QueryClient();
+
 createRoot(rootElement).render(
   <StrictMode>
-    <ThemeProvider>
-      <ErrorBoundary>
-        <RouterProvider router={router} />
-      </ErrorBoundary>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <RouterProvider router={router} />
+        </ErrorBoundary>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
