ccac479ff9a000a9b1f82d9b9c25c1ab0150d8ce
Anna Vasilevich <dzichonka@gmail.com>
2025-08-16 15:44:20 +0200

    chore: make configuration for tailwind

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   output: 'export',
   distDir: './dist',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(process.cwd()),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
